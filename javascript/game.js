b11b0f750f50c9f68ecf5195b52ade2b987a7684
bterrier <joann_enos@verizon.net>
2018-01-07 09:21:43 -0500

    Modify margins, font-size and cleanup javascript functions.

--- javascript/game.js
+++ javascript/game.js
@@ -7,8 +7,8 @@ $(document).ready(function(){
 	var gemArr = [];
 	var messages;
 	messages = {
-		win: "Congratulations! You win!",
-		lose: "You lose! Play again.",
+		win: "Congratulations! You win! Play Again!",
+		lose: "Sorry, you lose! Try again!",
 		none: "",
 };
 
@@ -16,13 +16,10 @@ $(document).ready(function(){
 		// Get random number between 19 and 120 including 120 itself.
 		targetScore = Math.floor(Math.random() * 102) + 19;
 		totalScore = 0;
-		// $(".lose-messages").text("");
-		// $(".win-messages").text("");
 
 		for (i=0; i<=3; i++) {
 			gemArr[i] = Math.floor((Math.random() * 12) + 1);
 		}
-
 		console.log("targetScore: " + targetScore);
 		console.log("gemArr: " + gemArr);
 		console.log("totalScore " +totalScore)
@@ -31,14 +28,12 @@ $(document).ready(function(){
 		$("#totalscore").text("Your Total Score: " + totalScore);
 		$(".wins").text("Wins: " + wins);
 		$(".losses").text("Losses: " + losses);
-		$(".win-messages").text(messages.none);
-		$(".lose-messages").text(messages.none);
-
 	}
 
 	initializeGame();
 
 	function checkSum() {
+
 		if (totalScore == targetScore) {
 			wins++;
 			$(".wins").text("Wins: " + wins);
@@ -48,21 +43,21 @@ $(document).ready(function(){
 			losses++; 
 			$(".losses").text("Losses: " + losses); 
 			$(".lose-messages").text(messages.lose);
-			console.log(".lose-messages" + messages.lose);
+			console.log(".lose-messages: " + messages.lose);
+			initializeGame();
 		} 
 	}
 			
-			initializeGame();
-
-//clicking on a gem
-$(".gems").on("click", function() {
-	var indexOf = $(this).attr("value");
-	console.log(indexOf);
-	totalScore += gemArr[indexOf];
-	$("#totalscore").text("Your Total Score: " + totalScore);
-	checkSum();
-
-});
+// Result of clicking on a gem.
+	$(".gems").on("click", function() {
+		var gemIndexValue = $(this).attr("value");
+		console.log("gemIndexValue: " + gemIndexValue);
+		$(".lose-messages").text(messages.none);
+		$(".win-messages").text(messages.none);
+		totalScore += gemArr[gemIndexValue];
+		$("#totalscore").text("Your Total Score: " + totalScore);
+		checkSum();
+	});
 
 })
 
